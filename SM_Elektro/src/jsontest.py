a8e770ef8e85856a2be42273653aa7000d812405
antonmeyer <weltheimgong@gmail.com>
2019-07-14 17:29:03 +0200

    first json approach

---
+++ SM_Elektro/src/jsontest.py
@@ -0,0 +1,18 @@
+import requests
+import time
+
+sumVal = 0
+while sumVal < 320000:
+    try: 
+        r = requests.get('http://192.168.1.111/getdata')
+
+        jr = r.json()
+
+        m1 = jr['meters']['meter1']
+
+        sumVal = m1['sumVal']
+        print sumVal
+    except:
+        print "we have a problem"
+        time.sleep(1)
+
--- SM_Elektro/src/main.cpp
+++ SM_Elektro/src/main.cpp
@@ -1,58 +1,99 @@
 #include "WiFi.h"
-#include "aREST.h"
+#include <WebServer.h>
+#include <ArduinoJson.h>
 #include "wificonf.h"
- 
-aREST rest = aREST();
- 
-WiFiServer server(80);
- 
-const char* ssid = NETWORKNAME;
-const char* password =  NETWORKPASSWD;
- 
-int temperature;
+// auslagern des webzeugs #include "myWebserver.h"
+
+WebServer server(80);
+
+const char *ssid = NETWORKNAME;
+const char *password = NETWORKPASSWD;
+
+int sumVal = 5800;
 int humidity;
 
-int testFunction(String command) {
-  Serial.println("Received rest request");
+void handleRoot()
+{
+
+  server.send(200, "text/plain", "hello from esp32!");
 }
- 
+
+void handleNotFound()
+{
+  
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  
+}
+
+void handlegetData()
+{
+  String jsonresponse;
+ StaticJsonDocument<512> doc;
+  JsonObject root = doc.to<JsonObject>();
+  JsonObject meters = root.createNestedObject("meters");
+  JsonObject meter1 = meters.createNestedObject("meter1");
+  meter1["manuf"] = "EMH";
+  meter1["serial"] = "1234567";
+  meter1["sumVal"] = sumVal++;
+  meter1["sumUnit"] = "kWh";
+  
+  JsonObject meter2 = meters.createNestedObject("meter2");
+  meter2["manuf"] = "Conrad";
+  meter2["serial"] = "765432";
+  meter2["sumCnt"] = sumVal++;
+  meter2["sumUnit"] = "10000/kWh";
+
+
+  serializeJson(root, jsonresponse);
+  server.send(200,"text/json",jsonresponse);
+  
+}
+
 void setup()
 {
- 
+
   Serial.begin(115200);
- 
-  rest.function("test",testFunction);
- 
+
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
- 
+
   Serial.println("WiFi connected with IP: ");
   Serial.println(WiFi.localIP());
- 
 
-  rest.set_id("1");
-  rest.set_name("meinesp32");
+  server.on("/", handleRoot);
 
-  temperature = 24;
-  humidity = 40;
-  rest.variable("temperature",&temperature);
-  rest.variable("humidity",&humidity);
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
 
-   server.begin();
+  server.on("/getdata", handlegetData);
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
  
 }
- 
-void loop() {
- 
-  WiFiClient client = server.available();
-  if (client) {
- 
-    while(!client.available()){
-      delay(5);
-    }
-    rest.handle(client);
-  }
+
+void loop()
+{
+
+  server.handleClient();
 }
